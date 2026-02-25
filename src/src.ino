f5f27b8651e4f3be751145226618ac65d5a90ad1
Tomoooji <tomoji.k.2nd@gmail.com>
2026-02-25 20:54:50 +0900

    try iroiro..

--- src/src.ino
+++ src/src.ino
@@ -1,11 +1,22 @@
-#include "MotorDriver.h"
+#include "ESP32MotorDriver2pin.h"
+
+ESP32MotorDriver2pin leg1{,0,255};
+const uint8_t pin1 = 15;
+const uint8_t pin2 = 2;
+int flag;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  leg1.attach(pin1, pin2);
+  leg1.setVelocity(255);
+  leg1.stop();//init
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println("-----------");
+  //flag = leg1.setVelocity(250);
+  flag = leg1.velocity();
+  Serial.println(flag);
+  leg1.moveBackward();
+  delay(1000);
 }
